8f48392f94c470daba7281ea3091263c8de43a5c
PannaOta <3841168@gmail.com>
2024-02-27 17:55:16 +0200

    init commit

---
+++ HomeWorkJS1/js.js
@@ -0,0 +1,50 @@
+let a = 'hello';
+console.log(a)
+let a1;
+a1 = "owu";
+console.log(a1)
+let b = "com";
+console.log(b)
+let b1 = "ua";
+console.log(b1)
+let c = 1;
+console.log(c)
+let c1 = 10;
+console.log(c1);
+let d = -999;
+console.log(d);
+let d1 = 123;
+console.log(d1);
+let d2 = 3.14;
+console.log(d2);
+let d3 = 2.7;
+console.log(d3);
+let d4 = 16;
+console.log(d4);
+let x = true;
+console.log(x)
+let x1 = false;
+console.log(x1)
+
+//------------------------------------------------------------------------------------
+let firstNane = "Iryna";
+let middleName = "Petrivna";
+let lastName = "Kolodochka";
+let Res =  `${firstNane} ${middleName} ${lastName}`
+console.log(Res)
+
+let s = 100;
+console.log(typeof s)
+let s1 = `100`;
+console.log(typeof s1)
+let s2 = true;
+console.log(typeof s2)
+// -----------------------------------------
+let s3 = prompt(`Ірина`);
+console.log(s3)
+let s4 = prompt (`Петрівна`);
+console.log(s4)
+let s5 = prompt(`Колодочка`);
+console.log(s5)
+
+
